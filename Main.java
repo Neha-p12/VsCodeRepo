6116a02c747703caf16eeb91aa9a7e70ab763328
Neha-p12 <nehapawara2779@gmail.com>
2024-02-11 21:56:58 +0530

    Update Main.java
    commit n

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main
     {
         System.out.println("Hello");
         System.out.println("Main Class from vscode");
-        System.out.println("connect with git");
+        System.out.println("connect with a git");
     }
 }
