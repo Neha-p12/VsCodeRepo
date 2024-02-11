e047888b0ec109c391f31c87cbb4b981e78b455a
Neha-p12 <nehapawara2779@gmail.com>
2024-02-11 21:30:19 +0530

    first commit

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ public class Main
     public static void main(String[]args)
     {
         System.out.println("Hello");
-        System.out.println("Main Class");
+        System.out.println("Main Class from vscode");
     }
 }
